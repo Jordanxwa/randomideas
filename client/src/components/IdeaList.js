8e1e1652de059cbd1a2b4470fe6140536143cdca
Jordan Wallace-Addai <jordan-wallace-addai@mac.home>
2024-11-19 21:02:58 -0500

    added delete funtion

--- client/src/components/IdeaList.js
+++ client/src/components/IdeaList.js
@@ -1,3 +1,4 @@
+import ideasApi from '../services/ideasApi';
 import IdeasApi from '../services/ideasApi';
 
 class IdeaList {
@@ -14,16 +15,37 @@ class IdeaList {
     this._validTags.add('inventions');
   }
 
+  addEventListeners() {
+    this._ideaListEl.addEventListener('click', (e) => {
+      if (e.target.classList.contains('fa-times')) {
+        e.stopImmediatePropagation();
+        const ideaId = e.target.parentElement.parentElement.dataset.id;
+        this.deleteIdea(ideaId);
+      }
+    });
+  }
+
   async getIdeas() {
     try {
       const res = await IdeasApi.getIdeas();
       this._ideas = res.data.data;
-      console.log(this._ideas);
       this.render();
     } catch (error) {
       console.log(error);
     }
   }
+
+  async deleteIdea(ideaId) {
+    try {
+      // Delete from server
+      const res = await ideasApi.deleteIdea(ideaId);
+      this._ideas.filter((idea) => idea._id !== ideaId);
+      this.getIdeas();
+    } catch (error) {
+      alert("Can't delete this idea");
+    }
+  }
+
   addIdeaToList(idea) {
     this._ideas.push(idea);
     this.render();
@@ -44,9 +66,13 @@ class IdeaList {
     this._ideaListEl.innerHTML = this._ideas
       .map((idea) => {
         const tagClass = this.getTagClass(idea.tag);
+        const deleteBtn =
+          idea.username === localStorage.getItem('username')
+            ? `<button class="delete"><i class="fas fa-times"></i></button>`
+            : '';
         return `
-         <div class="card">
-        <button class="delete"><i class="fas fa-times"></i></button>
+         <div class="card" data-id="${idea._id}">
+        ${deleteBtn}
         <h3>
        ${idea.text}
         </h3>
@@ -59,6 +85,7 @@ class IdeaList {
       `;
       })
       .join('');
+    this.addEventListeners();
   }
 }
 
